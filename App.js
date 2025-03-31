b8be2bb32e145f18a497bedc8764a1fcacfba4b6
AkhilaBestha <akhila.bestha7013@gmail.com>
2025-03-31 10:10:20 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Product from "./Product";
+
+function App() {
+    return (
+        <div>
+            <h1>Product List</h1>
+            <Product />
+        </div>
+    );
+}
+
+export default App;
